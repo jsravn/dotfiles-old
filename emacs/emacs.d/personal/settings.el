8e4a9a264955625a51bd5e4c60221a544483d995
James Ravn <james@r-vn.org>
2015-05-18 08:37:03 +0100

    Add rust mode and some other bits and bobs

--- emacs/emacs.d/personal/settings.el
+++ emacs/emacs.d/personal/settings.el
@@ -8,20 +8,22 @@
 (setq x-select-enable-clipboard t)
 (setq ido-use-virtual-buffers t)
 
-(prelude-require-packages '(evil markdown-mode robe enh-ruby-mode))
+(prelude-require-packages '(evil markdown-mode robe enh-ruby-mode neotree rust-mode))
 
 (evil-mode 1)
 
 ;; evil mode tweaks per major mode
 (evil-set-initial-state 'haskell-interactive-mode 'emacs)
 (evil-set-initial-state 'git-commit-mode 'insert)
+(evil-set-initial-state 'neotree-mode 'emacs)
+(evil-set-initial-state 'shell-mode 'emacs)
 
 (prelude-mode 1)
 
 (setq default-frame-alist '((font . "Droid Sans Mono Slashed:size=15")))
 (setq prelude-flyspell nil)
 (setq browse-url-browser-function 'browse-url-chromium)
-
+(setq magit-last-seen-setup-instructions "1.4.0")
 ;; http://comments.gmane.org/gmane.comp.lang.haskell.cafe/85859
 ;(defadvice inferior-haskell-load-file (after change-focus-after-load)
 ;  "Change focus to GHCi window after C-c C-l command"
@@ -34,5 +36,9 @@
 (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
 
 (setq org-export-backends '(docbook html beamer ascii latex md))
+
+;; neotree
+(global-set-key [f8] 'neotree-toggle)
+(setq projectile-switch-project-action 'neotree-projectile-action)
 
 ;;; settings.el ends here
