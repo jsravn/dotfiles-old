a69a7a5fb5ca238a8690e0b75083ce23fb9887ee
James Ravn <james@r-vn.org>
2016-01-01 13:06:54 +0000

    Update emacs.d

--- emacs/emacs.d/prelude-modules.el
+++ emacs/emacs.d/prelude-modules.el
@@ -19,7 +19,7 @@
 ;; (require 'prelude-css)
 (require 'prelude-emacs-lisp)
 ;; (require 'prelude-erlang)
-;; (require 'prelude-go)
+(require 'prelude-go)
 (require 'prelude-haskell)
 (require 'prelude-js)
 ;; (require 'prelude-latex)
@@ -27,7 +27,7 @@
 ;; (require 'prelude-ocaml)
 (require 'prelude-org) ;; Org-mode helps you keep TODO lists, notes and more
 (require 'prelude-perl)
-;; (require 'prelude-python)
+(require 'prelude-python)
 (require 'prelude-ruby)
 ;; (require 'prelude-scala)
 (require 'prelude-scheme)
