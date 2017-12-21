38241d0136817d3ef8ce55c04d0b754402e1e6b5
James Ravn <james@r-vn.org>
2017-12-21 14:41:16 -0800

    Use intero for emacs haskell

--- emacs/emacs.d/personal/settings.el
+++ emacs/emacs.d/personal/settings.el
@@ -7,7 +7,7 @@
 (setq x-select-enable-primary t)
 (setq x-select-enable-clipboard t)
 (setq ido-use-virtual-buffers t)
-(prelude-require-packages '(evil markdown-mode robe enh-ruby-mode rust-mode ghc company-ghc hindent auctex))
+(prelude-require-packages '(evil markdown-mode robe enh-ruby-mode rust-mode ghc company-ghc hindent auctex intero))
 (evil-mode 1)
 (add-hook 'after-init-hook 'global-company-mode)
 (setq visible-bell 1)
@@ -15,6 +15,9 @@
 (setq browse-url-browser-function 'browse-url-chromium)
 (setq magit-last-seen-setup-instructions "1.4.0")
 (setq linum-format "%4d \u2502 ")
+(let ((my-local-path (expand-file-name "~/.local/bin")))
+  (setenv "PATH" (concat my-local-path path-separator (getenv "PATH")))
+  (add-to-list 'exec-path my-local-path))
 
 ;; evil mode tweaks per major mode
 (evil-set-initial-state 'haskell-interactive-mode 'emacs)
@@ -22,6 +25,7 @@
 (evil-set-initial-state 'git-commit-mode 'insert)
 (evil-set-initial-state 'neotree-mode 'emacs)
 (evil-set-initial-state 'shell-mode 'emacs)
+(evil-set-initial-state 'intero-repl-mode 'emacs)
 (define-key evil-normal-state-map (kbd "C-u") 'evil-scroll-up)
 
 ;; prelude tweaks
@@ -33,39 +37,11 @@
 (add-hook 'enh-ruby-mode-hook 'robe-mode)
 
 ;; haskell
-;; see https://github.com/serras/emacs-haskell-tutorial/blob/master/tutorial.md
-(let ((my-stackage-path (expand-file-name "~/.local/bin")))
-  (setenv "PATH" (concat my-stackage-path path-separator (getenv "PATH")))
-  (add-to-list 'exec-path my-stackage-path))
+(add-hook 'haskell-mode-hook (lambda ()
+                               (intero-mode)
+                               (hindent-mode)))
 (custom-set-variables
- '(haskell-process-suggest-remove-import-lines t)
- '(haskell-process-auto-import-loaded-modules t)
- '(haskell-process-log t)
- '(haskell-tags-on-save t)
- '(haskell-process-type 'stack-ghci))
-(eval-after-load 'haskell-mode '(progn
-  (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
-  (define-key haskell-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
-  (define-key haskell-mode-map (kbd "C-c C-n C-t") 'haskell-process-do-type)
-  (define-key haskell-mode-map (kbd "C-c C-n C-i") 'haskell-process-do-info)
-  (define-key haskell-mode-map (kbd "C-c C-n C-c") 'haskell-process-cabal-build)
-  (define-key haskell-mode-map (kbd "C-c C-o") 'haskell-compile)
-  (define-key haskell-mode-map (kbd "C-c C-n c") 'haskell-process-cabal)))
-(eval-after-load 'haskell-cabal '(progn
-  (define-key haskell-cabal-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
-  (define-key haskell-cabal-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
-  (define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
-  (define-key haskell-cabal-mode-map (kbd "C-c C-o") 'haskell-compile)
-  (define-key haskell-cabal-mode-map (kbd "C-c c") 'haskell-process-cabal)))
-(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
-(autoload 'ghc-init "ghc" nil t)
-(autoload 'ghc-debug "ghc" nil t)
-(add-hook 'haskell-mode-hook (lambda () (ghc-init)))
-(add-to-list 'company-backends 'company-ghc)
-(custom-set-variables '(company-ghc-show-info t))
-(eval-after-load 'haskell-mode
-  '(define-key haskell-mode-map [f8] 'haskell-navigate-imports))
-(add-hook 'haskell-mode-hook 'hindent-mode)
+ '(haskell-stylish-on-save t))
 
 ;; org
 (setq org-export-backends '(docbook html beamer ascii latex md))
